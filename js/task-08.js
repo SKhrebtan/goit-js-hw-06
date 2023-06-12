36d91e67193a3f07d4424c6ce09055cc53387f11
SKhrebtan <shrebtan@gmail.com>
2023-06-12 19:27:34 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+   
+    if (email === '' || password === '') {
+        alert('Будьте ласкаві, заповніть всі поля')
+    }
+            const formData = {
+            email,
+            password,
+        }
+   
+    console.log(formData);
+    
+    event.currentTarget.reset();
+}
+   
